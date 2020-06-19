7d49a0d7dc0dcbfd0678b3c46302ae0e8c2cae36
danjones1618 <hello@danjones.dev>
2020-06-19 16:29:46 +0100

    Updates

---
+++ precache-manifest.282624fa5ceac7f560d79d201feb002d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f80801fd63e2fd08b82717dcfca300",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5c21e7c5abdf075cf95",
+    "url": "/static/css/main.dd013594.chunk.css"
+  },
+  {
+    "revision": "c0a8a283098eedf885b4",
+    "url": "/static/js/2.313c4ae1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.313c4ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c21e7c5abdf075cf95",
+    "url": "/static/js/main.c3f5a609.chunk.js"
+  },
+  {
+    "revision": "1b987f8090293c80ae71",
+    "url": "/static/js/runtime-main.5b7a3fff.js"
+  }
+]);
